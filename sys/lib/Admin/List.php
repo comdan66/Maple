1883262d3ac8038d195423bd28db51bcec4df99a
OA Wu <comdan66@gmail.com>
2018-08-27 13:10:38 +0800

    👨🏻‍💼 後台管理介面

--- sys/lib/Admin/List.php
+++ sys/lib/Admin/List.php
@@ -4,7 +4,7 @@ class AdminList {
   const SORT_KEY = '_s';
   const SEARCH_KEY = '_q';
 
-  private $model, $option = [], $addUrl, $sortUrl, $where, $runQuery = false, $total = 0, $objs = [], $trsIndex = 0, $trs = [], $searches = [], $searchStr, $tableStr, $pagesStr;
+  private $model, $option = [], $url, $addUrl, $sortUrl, $where, $runQuery = false, $total = 0, $objs = [], $trsIndex = 0, $trs = [], $searches = [], $searchStr, $tableStr, $pagesStr;
 
   public static function create($model, $option = []) {
     return new static($model, $option);
@@ -20,6 +20,11 @@ class AdminList {
 
   public function &objs() {
     return $this->query()->objs;
+  }
+
+  public function setUrl($url) {
+    $this->url = $url;
+    return $this;
   }
 
   public function setAddUrl($addUrl) {
@@ -124,7 +129,7 @@ class AdminList {
     $this->searchStr .= '<form class="search" action="' . Url::current() . '" method="get">';
       $this->searchStr .= '<div class="info' . ($titles ? ' show' : '') . '">';
         $this->searchStr .= '<a class="icon-13 conditions-btn"></a>';
-        $this->searchStr .= '<span>' . ($this->addUrl ? '<a href="' . $this->addUrl . '" class="icon-07">新增</a>' : '') . ($sortKey ? '<a href="' . $sortKey . '" class="icon-' . ($ing ? '41' : '18') . '">' . ($ing ? '排序' : '完成') . '</a>' : '') . '</span>';
+        $this->searchStr .= '<span>' . ($this->url ? $this->url : '') . ($this->addUrl ? '<a href="' . $this->addUrl . '" class="icon-07">新增</a>' : '') . ($sortKey ? '<a href="' . $sortKey . '" class="icon-' . ($ing ? '41' : '18') . '">' . ($ing ? '排序' : '完成') . '</a>' : '') . '</span>';
         $this->searchStr .= '<span>' . ($titles ? '您針對' . implode('、', array_map(function($title) { return '「' . $title . '」'; }, $titles)) . '搜尋，結果' : '目前全部') . '共有 <b>' . number_format($this->total) . '</b> 筆。' . '</span>';
       $this->searchStr .= '</div>';
       $this->searchStr .= '<div class="conditions">';
