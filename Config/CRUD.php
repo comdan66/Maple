aa47afaa1c717777c4178b8732b5857f0d5a7568
OA Wu <comdan66@gmail.com>
2019-07-18 16:45:23 +0800

    修正指令

--- Config/CRUD.php
+++ Config/CRUD.php
@@ -8,7 +8,9 @@ return [
   //   'controllerName' => '', // Article
   //   'modelName'      => '', // \M\Article
   // ],
-  
+
+  'dir' => '',   // 目錄
+
   'title'          => '',   // 文章管理
   'routerUri'      => '',   // articles
   'controllerName' => '',   // Article
@@ -17,12 +19,12 @@ return [
   'sort'           => true, // 是否有「排序」的功能，sort 欄位
 
   'images' => [
-    // ['must' => true, 'name' => 'cover', 'text' => '封面', 'accept' => 'image/*', 'formats' => ['jpg', 'png', 'jpeg']]
+    // ['must' => true, 'name' => 'cover', 'text' => '封面', 'accept' => 'image/*', 'formats' => ['jpg', 'png', 'jpeg']],
   ],
   'texts' => [
-    // ['must' => true, 'name' => 'title', 'text' => '標題', 'type' => 'text']
+    // ['must' => true, 'name' => 'title', 'text' => '標題', 'type' => 'text'],
   ],
   'textareas' => [
-    // ['must' => true, 'name' => 'content', 'text' => '內容', 'type' => 'ckeditor']
+    // ['must' => true, 'name' => 'content', 'text' => '內容', 'type' => 'ckeditor'],
   ],
 ];
